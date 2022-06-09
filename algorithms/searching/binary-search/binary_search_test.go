0f770fc3648b3616bc7b436fb228818976e5220a
Lucas Dinnouti <lucas.dinnouti@wavy.global>
2022-06-08 22:44:04 -0300

    testing binary_search

--- algorithms/searching/binary-search/binary_search_test.go
+++ algorithms/searching/binary-search/binary_search_test.go
@@ -3,6 +3,8 @@ package bs
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSearch(t *testing.T) {
@@ -24,4 +26,46 @@ func TestSearch(t *testing.T) {
 	if search(sorted, 3) != -1 {
 		t.Error()
 	}
+}
+
+func TestFindFirstElement(t *testing.T) {
+	sorted := make([]int, 100)
+
+	for i := 0; i < 100; i++ {
+		sorted[i] = 2 * i
+	}
+
+	result := search(sorted, 0)
+
+	expected_response := 0
+
+	assert.Equal(t, expected_response, result, "The left-most element could not be found")
+}
+
+func TestFindLastElement(t *testing.T) {
+	sorted := make([]int, 100)
+
+	for i := 0; i < 100; i++ {
+		sorted[i] = 2 * i
+	}
+
+	result := search(sorted, 198)
+
+	expected_response := 99
+
+	assert.Equal(t, expected_response, result, "The right-most element could not be found")
+}
+
+func TestDoNotFindUnexistingElement(t *testing.T) {
+	sorted := make([]int, 100)
+
+	for i := 0; i < 100; i++ {
+		sorted[i] = 2 * i
+	}
+
+	result := search(sorted, 200)
+
+	expected_response := -1
+
+	assert.Equal(t, expected_response, result, "The unexisting element was found")
 }
